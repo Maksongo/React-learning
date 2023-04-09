da2c54cab2b31a744a26f200d342ebee01d08d5d
Makson <nooplace55@gmail.com>
2023-04-09 22:36:58 +0600

    write some dynamic values

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -8,8 +8,15 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <h1> { title }</h1>
-        <p>Liked { likes } times</p>
+        <h1> {title}</h1>
+        <p>Liked {likes} times</p>
+
+        {/* <p> { person } </p> */}
+
+        <p>{ 10 }</p>
+        <p>{ 'hello ninjas!' }</p>
+        <p>{ [1,2,3,4,5]}</p>
+        <p>{ Math.random() * 10}</p>
       </div>
     </div>
   );
